0d8431650cb3338e85ee12f355bc801e94a5558d
THỊNH IT <87349778+Robert-Ph@users.noreply.github.com>
2021-12-17 14:57:51 +0700

    list

---
+++ ListStudent/Student.java
@@ -0,0 +1,24 @@
+package ListStudent;
+
+public class Student {
+	private String name;
+	private int id;
+	private int age;
+	private String address;
+	private double diemTrungBinh;
+	
+	public Student(String name, int id, int age, String address, double diemTrungBinh) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.age = age;
+		this.address = address;
+		this.diemTrungBinh = diemTrungBinh;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ListStudent/StudentManagement.java
@@ -0,0 +1,42 @@
+package ListStudent;
+
+import java.util.ArrayList;
+
+public class StudentManagement {
+	private ArrayList<Student> listStudent = new ArrayList<Student>();
+	
+	
+	// hien thi danh sach sinh vien
+	public static String showListStudent() {
+		
+		return null;
+	}
+	// cap nhat danh sach sinh vien
+	public static ArrayList<Student> updatestudent(Student stu){
+		
+		return null;
+	}
+	// xoa sinh vien theo id
+	public  void delStudent(int id) {
+		
+	}
+	//tim kiem sinh vien theo id
+	public static Student searchStudent(int id) {
+		
+		return null;
+	}
+	// sap xep sinh vien theo diem trung binh
+	public static ArrayList<Student> sortStudent(){
+		
+		return null;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student stu1 = new Student(null, 0, 0, null, 0);
+		Student stu2 = new Student(null, 0, 0, null, 0);
+		Student stu3 = new Student(null, 0, 0, null, 0);
+		Student stu4 = new Student(null, 0, 0, null, 0);
+		Student stu5 = new Student(null, 0, 0, null, 0);
+	}
+
+}
