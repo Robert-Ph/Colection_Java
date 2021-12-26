769fe52394abd5b4bfabab5b675a5ce4335acd4b
THỊNH IT <87349778+Robert-Ph@users.noreply.github.com>
2021-12-26 15:57:10 +0700

    Update TraTuDien.java

--- TraTuDien.java
+++ TraTuDien.java
@@ -13,7 +13,7 @@ public class TraTuDien {
     public TraTuDien(Map<String, String> tuDien) {
         this.tuDien = tuDien;
     }
-
+    // đọc file từ điển
     public  void read(String url) {
         try {
             FileReader r = new FileReader(url);
@@ -37,14 +37,29 @@ public class TraTuDien {
     public void xoaTu(String key) {
         this.tuDien.remove(key);
     }
-
-    public String traTu(String key) {
+    // tra từ
+    public String traTu(String tu) {
+    	String result ="";
+    	String h = "Khong tim thay!";
+    	if(!(traTuAnhViet(tu)).equals(h)) {
+    		result = traTuAnhViet(tu);
+    	}else if(!(traTuVietAnh(tu)).equals(h)) {
+    		result = traTuVietAnh(tu);
+    	}else if(!(traTuTuongDong(tu)).equals(h)) {
+    		result = traTuTuongDong(tu);
+    	}else {
+    		return h;
+    	}
+    	return result;
+    }
+    // tra từ Anh - Việt
+    public String traTuAnhViet(String key) {
     	 String result = "";
     	String k = inHoa(key);
         Set<String> set = tuDien.keySet();
         for (String i : set) {
             if (i.equals(k)) {
-                result = i + " : " + this.tuDien.get(i);
+                result = key + " : " + this.tuDien.get(i);
                 break;
             } else {
                 result = "Khong tim thay!";
@@ -52,7 +67,40 @@ public class TraTuDien {
         }
         return result;
     }
-
+    // Tra từ Việt - Anh
+    public String traTuVietAnh(String value) {
+    	String k = value.toLowerCase();
+    	String result ="";
+    	Set<String> set = tuDien.keySet();
+    	for(String i: set) {
+    		if(tuDien.get(i).equals(k)) {
+    			 result = value + " : " + i;
+                 break;
+    		}else {
+    			result = "Khong tim thay!";
+    		}
+    	}
+    	return result;
+    }
+    // tra những tù có chữ cái bắt đầu do người dùng nhập vào
+    public String traTuTuongDong(String key) {
+    	String result ="";
+    	String k = inHoa(key);
+    	Set<String> set = tuDien.keySet();
+    	for(String i: set) {
+    		String gt ="";
+    		char[] s = i.toCharArray();
+    		for(int j=0; j < key.length();j++) {
+    			gt +=String.valueOf(s[j]);
+    		}
+    		if(gt.equals(k)) {
+    			result += i +" : " + tuDien.get(i) + "\n";
+    		}
+    		
+    	}
+    	return result;
+    }
+    
     public String toString() {
         String result = "";
         Set<String> set = tuDien.keySet();
@@ -83,8 +131,7 @@ public class TraTuDien {
         tra.themTu("Dog", "con cho");
         tra.xoaTu("Cat");
         System.out.println(tra.toString());
-        System.out.println(tra.traTu("Hello"));
-        
+        System.out.println(tra.traTu("c"));
         
     }
 }
